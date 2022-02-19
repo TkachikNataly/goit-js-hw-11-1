3bf4ab11b6405f1193176d222dc578f2cbb20000
TkachikNataly <tkachik.nataly@gmail.com>
2022-02-19 20:11:16 +0200

    Gallery

---
+++ src/js/api-servic.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/';
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const options = {
+      params: {
+        key: '25760251-3970e02ca371c5f8341f48ac2',
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: this.page,
+      },
+    };
+
+    const response = await axios.get('api/', options);
+    const dataImages = await response.data;
+    this.incrementPage();
+    return dataImages;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
